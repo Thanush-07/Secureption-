eadcfe886c9dbe12b4a818b8b26f5681ebe40a64
Thanush-07 <thanushkumar722@gmail.com>
2025-07-13 01:34:08 -0400

    app file updated

--- app.py
+++ app.py
@@ -6,5 +6,21 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/encrypt')
+
+def encrypt():
+    return render_template('encrypt.html')
+
+@app.route('/decrypt')
+
+def decrypt():
+    return render_template('decrypt.html')
+
+@app.route('/instruction')
+
+def instruction():
+   return render_template('instruction.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
